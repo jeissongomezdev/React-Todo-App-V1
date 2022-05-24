ebf03f9d63b3251daf37a3b03f9a663cea8af744
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-24 16:36:29 -0500

    Iniciando el proyecto :smile:

---
+++ README.md
@@ -0,0 +1,3 @@
+## TODO APP en React.Js ⚛
+
+Aplicación de tareas en la cual podemos crear tareas y eliminarlas.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
