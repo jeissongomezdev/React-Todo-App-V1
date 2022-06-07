cf8bc63e5aa9eaeedfa0c45294ce01e80c5d42ff
Jeisson Gómez <jeissongomez@hotmail.es>
2022-06-06 21:43:05 -0500

    Creando la App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  //Tarea que agregamos
+  const [todo, setTodo] = useState("");
+
+  //Lista de tareas
+  const [todos, setTodos] = useState([]);
+
+  //Cancelamos que el formulario recargue la pagina
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const addTodo = () => {
+    let updateTodos = [
+      ...todos,
+      {
+        title: todo,
+        completed: false,
+      },
+    ];
+    setTodos(updateTodos);
+  };
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <div className="todo">
+        <h1 className="todo__title">Mis Tareas</h1>
+        <form className="todo__input-container" onSubmit={handleSubmit}>
+          <input
+            className="todo__input"
+            type="text"
+            value={todo}
+            name="text"
+            placeholder="Agrega una nueva tarea aquí..."
+            autoComplete="off"
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button className="todo__add-btn" onClick={addTodo}>
+            Agregar
+          </button>
+        </form>
+        <div className="todolist-container">
+          <div className="todos-container">
+            <div>
+              {todos.map((todo) => (
+                <div>{todo}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
